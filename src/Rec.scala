cf99ab76cb29eeabdc0df7ed499ce011f39d03ed
Hubert Plociniczak <hubert.plociniczak@gmail.com>
2014-06-17 15:24:07 +0200

    Minor corrections to manual building, some examples

--- src/Rec.scala
+++ src/Rec.scala
@@ -8,20 +8,44 @@ object Main extends App {
   type Fields = (String, Type)
   type Schema = List[Fields]
   def create(s: Schema): Tree = {
-    def  fieldTree(field: (String, reflect.runtime.universe.Type)) = ValDef(Modifiers(), TermName(field._1), Ident(TypeName(field._2.toString)), TypeApply(Select(Apply(Ident(TermName("list")), List(Literal(Constant(0)))), TermName("asInstanceOf")), List(Ident(TypeName(field._2.toString)))))
-    val const = DefDef(Modifiers(), termNames.CONSTRUCTOR, List(), List(List()), TypeTree(), Block(Nil, Literal(Constant(()))))
+    def  fieldTree(field: (String, reflect.runtime.universe.Type)) =
+      ValDef(Modifiers(), TermName(field._1), Ident(TypeName(field._2.toString)),
+          TypeApply(Select(Apply(Ident(TermName("list")), List(Literal(Constant(0)))), TermName("asInstanceOf")), List(Ident(TypeName(field._2.toString)))))
+    val const = DefDef(Modifiers(), termNames.CONSTRUCTOR, List(), List(List()), TypeTree(), Block(List(pendingSuperCall), Literal(Constant(()))))
     val fields = s.map(fieldTree)
- 	Function(
- 		List(ValDef(Modifiers(PARAM), TermName("list"), AppliedTypeTree(Ident(TypeName("List")), List(Ident(TypeName("Any")))), EmptyTree)),
- 		 Block(List(ClassDef(Modifiers(FINAL), TypeName("$anon"), List(),
- 		 	Template(List(Ident(TypeName("Rec"))), noSelfType, const :: fields))),
- 		    Apply(Select(New(Ident(TypeName("$anon"))), termNames.CONSTRUCTOR), List())))
+   	val fun = 
+     Function(
+   		List(ValDef(Modifiers(PARAM), TermName("list"), AppliedTypeTree(Ident(TypeName("List")), List(Ident(TypeName("Any")))), EmptyTree)),
+   		 Block(List(ClassDef(Modifiers(FINAL), TypeName("$anon"), List(),
+   		 	Template(List(Ident(TypeName("Rec"))), noSelfType, const :: fields))),
+   		    Apply(Select(New(Ident(TypeName("$anon"))), termNames.CONSTRUCTOR), List())))
+    fun
+  }
+
+  def prog(fun: Tree, values: Tree, name: String) = {
+    val recTrait = ClassDef(Modifiers(ABSTRACT | INTERFACE | DEFAULTPARAM | TRAIT),
+                            TypeName("Rec"), List(),
+                              Template(List(Select(Ident("scala"), TypeName("AnyRef"))), noSelfType, List()))
+    Block(List(recTrait), Select(Apply(fun, values :: Nil), name))
   }
 
 
 
   val tree = create(("phone", typeOf[String]) :: ("age", typeOf[Int]) :: Nil)
+  val values = q"""("072929292", 12) :: ("138838383", 0) :: Nil"""
+  val exec = prog(tree, values, "what")
 
   val tb = m.mkToolBox()
-  println(tb.typecheck(tree))
+  val programs = (prog(tree, values, "what"), false) ::
+                 (prog(tree, values, "phone"), true) :: Nil
+  programs foreach { case (prog, correct) =>
+    try {
+      println("Record select: ")
+      println(showCode(prog))
+      tb.typecheck(prog)
+      if (!correct) throw new Exception("fail")
+    } catch {
+      case (_: scala.tools.reflect.ToolBoxError) if !correct => ()
+    }
+  }
 }
