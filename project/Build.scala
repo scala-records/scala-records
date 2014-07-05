b9c2bf0c850ddf13a2e96fb252f16895b3885a6d
Tobias Schlatter <tobias@meisch.ch>
2014-07-05 17:25:41 +0200

    Add 2.12.0-SNAPSHOT to Scala version (fixes #6)

--- project/Build.scala
+++ project/Build.scala
@@ -11,14 +11,23 @@ object BuildSettings {
     scalacOptions ++= Seq("-deprecation", "-feature"),
     autoAPIMappings := true,
     scalaVersion := "2.11.1",
-    crossScalaVersions := Seq("2.10.2", "2.10.3", "2.10.4", "2.11.0", "2.11.1"),
-    libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+    crossScalaVersions := Seq(
+      "2.10.2", "2.10.3", "2.10.4",
+      "2.11.0", "2.11.1",
+      "2.12.0-SNAPSHOT"),
+    resolvers += Resolver.sonatypeRepo("snapshots"),
+    libraryDependencies += {
+      if (scalaVersion.value == "2.12.0-SNAPSHOT")
+        "org.scalatest" % "scalatest_2.11" % "2.2.0" % "test"
+      else
+        "org.scalatest" %% "scalatest" % "2.2.0" % "test"
+    }
   )
 
   val macroBuildSettings = buildSettings ++ Seq(
     libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-reflect" % _),
     libraryDependencies ++= {
-      if (scalaVersion.value.startsWith("2.10")) Seq(
+      if (scalaBinaryVersion.value == "2.10") Seq(
           compilerPlugin("org.scalamacros" % "paradise" % "2.0.0" cross CrossVersion.full),
           "org.scalamacros" %% "quasiquotes" % "2.0.0" cross CrossVersion.binary
       ) else Nil
@@ -58,7 +67,8 @@ object MyBuild extends Build {
     file("core"),
     settings = buildSettings ++ Seq(
       unmanagedSourceDirectories in Test ++= {
-        if (scalaVersion.value.startsWith("2.11"))
+        if (scalaBinaryVersion.value == "2.11" ||
+            scalaVersion.value == "2.12.0-SNAPSHOTS")
           Seq(sourceDirectory.value / "test-2.11" / "scala")
         else
           Seq()
