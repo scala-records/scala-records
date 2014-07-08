bffea4355b43f8a8be4025161f0035c8d88a9822
Vojin Jovanovic <gvojin@gmail.com>
2014-07-08 10:08:04 +0200

    No need for materializers.

--- common/src/main/scala/records/R.scala
+++ common/src/main/scala/records/R.scala
@@ -1,13 +1,13 @@
 package records
 
 import scala.language.experimental.macros
+import reflect.macros.blackbox.Context
 
 object R {
   def apply(v: (String, Any)*): R = macro records.Macros.apply_impl
 
-  implicit class ConvertR[From <: R](val r: From) {
-    import RecordConversions._
-    def to[To](implicit conv: FromRecord[From, To]): To = conv(r)
+  implicit class ConvertR[From <: R](val record: From) extends AnyVal {
+    def to[To]: To = macro RecordConversions.fromRecord_impl[From, To]        
   }
 }
 
